1a54af7bc1be2022bb8443a6417b62f00a4dcb0b
范辉 <fanhui.x@gmail.com>
2020-06-30 18:55:39 +0800

    add command-line args

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,79 @@
+use clap::App;
 use myutil::{err::*, *};
 use nix::{
     mount::{self, umount2, MntFlags, MsFlags},
-    sched::{clone, unshare, CloneFlags},
+    sched::{clone, CloneFlags},
     unistd::{chdir, execv, pivot_root},
 };
-use std::ffi::CString;
+use std::{ffi::CString, process};
 
-fn main() {
-    // TODO clap args
-    pnk!(run("/data/baseimage", "/sbin/init"));
+macro_rules! err {
+    () => {{
+        eprintln!("\n\x1b[31;01mInvalid arguments, please run `nspawn-lite --help`.\x1b[00m");
+        process::exit(1);
+    }};
 }
 
-fn run(root_path: &str, cmd_path: &str) -> Result<i32> {
-    let ops = || -> isize {
-        let res = mount_make_rprivate()
-            .c(d!())
-            .and_then(|_| do_pivot_root(root_path).c(d!()))
-            .and_then(|_| mount_dynfs_proc().c(d!()))
-            .and_then(|_| unshare(CloneFlags::CLONE_NEWUSER).c(d!()))
-            .and_then(|_| start_systemd(cmd_path));
-        pnk!(res);
-        0
-    };
+fn main() {
+    let matches = App::new("nspawn-lite")
+        .version("0.1")
+        .author("FanHui. <hui.fan@mail.ru>")
+        .about("A mininal container engine.")
+        .args_from_usage(
+            "-r --root-path=[PATH] 'The new rootfs path **before** chroot.'",
+        )
+        .args_from_usage(
+            "-c --cmd-path=[PATH] 'The command path **after** chroot.'",
+        )
+        .get_matches();
 
-    let mut stack = vec![0; 1024 * 1024];
+    match (matches.value_of("root-path"), matches.value_of("cmd-path")) {
+        (Some(rp), Some(cp)) => {
+            pnk!(run(rp, cp));
+        }
+        _ => {
+            err!();
+        }
+    }
+}
+
+// Return the PID of the-inner-systemd
+fn run(root_path: &str, cmd_path: &str) -> Result<i32> {
+    // 临时栈空间，执行`execv`后就会被丢弃
+    const STACK_SIZ: usize = 1024 * 1024;
+
+    let mut stack = Vec::with_capacity(STACK_SIZ);
+    unsafe {
+        stack.set_len(STACK_SIZ);
+    }
+
     let mut flags = CloneFlags::empty();
     flags.insert(CloneFlags::CLONE_NEWNS);
     flags.insert(CloneFlags::CLONE_NEWPID);
     flags.insert(CloneFlags::CLONE_NEWUTS);
     flags.insert(CloneFlags::CLONE_NEWIPC);
 
-    let pid = clone(
+    // Create a minimal container
+    let ops = || -> isize {
+        pnk!(mount_make_rprivate()
+            .c(d!())
+            .and_then(|_| do_pivot_root(root_path).c(d!()))
+            .and_then(|_| mount_dynfs_proc().c(d!()))
+            .and_then(|_| start_systemd(cmd_path)));
+        0
+    };
+
+    Ok(clone(
         Box::new(ops),
         stack.as_mut_slice(),
         flags,
         Some(libc::SIGCHLD),
     )
     .c(d!())?
-    .as_raw();
-
-    Ok(pid)
+    .as_raw())
 }
 
+// SEE: `man 2 pivot_root`
 fn mount_make_rprivate() -> Result<()> {
     mountx(
         None,
@@ -55,6 +87,7 @@ fn mount_make_rprivate() -> Result<()> {
     .c(d!())
 }
 
+// As the PID-1 process
 fn start_systemd(cmd_path: &str) -> Result<()> {
     execv(
         &CString::new(cmd_path).unwrap(),
@@ -67,6 +100,7 @@ fn start_systemd(cmd_path: &str) -> Result<()> {
     .c(d!())
 }
 
+// SEE: `man 2 pivot_root`
 fn do_pivot_root(root_path: &str) -> Result<()> {
     mountx(Some(root_path), root_path, None, MsFlags::MS_BIND, None)
         .c(d!())
